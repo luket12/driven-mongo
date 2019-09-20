a00622356e252a7902bb06955acbf30ab330dffc
Luke Taaffe <luke@one.fit>
2019-09-20 18:43:29 +0200

    Add note markup

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,55 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
-      <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-      <ul class="navbar-nav px-3">
-        <li class="nav-item text-nowrap">
-          <a class="nav-link" href="#">Sign out</a>
-        </li>
-      </ul>
-    </nav>
+    <div class="container-fluid">
+      <div class="row">
+        <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+          <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
+          <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+          <ul class="navbar-nav px-3">
+            <li class="nav-item text-nowrap">
+              <a class="nav-link" href="#">Sign out</a>
+            </li>
+          </ul>
+        </nav>
+        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+          <div class="sidebar-sticky">
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  <span data-feather="home"></span>
+                  <router-link to="/">Home</router-link>
+                  <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="file"></span>
+                  <router-link to="/note/add">Add Note</router-link>
+                </a>
+              </li>
+            </ul>
 
-    <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-      <div class="sidebar-sticky">
-        <ul class="nav flex-column">
-          <li class="nav-item">
-            <a class="nav-link active" href="#">
-              <span data-feather="home"></span>
-              <router-link to="/">Home</router-link>
-              <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="file"></span>
-              <router-link to="/note/add">Add Note</router-link>
-            </a>
-          </li>
-        </ul>
-
-        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-          <span>Additional Options</span>
-          <a class="d-flex align-items-center text-muted" href="#">
-            <span data-feather="plus-circle"></span>
-          </a>
-        </h6>
-        <ul class="nav flex-column mb-2">
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <span data-feather="file-text"></span>
-              My Profile
-            </a>
-          </li>
-        </ul>
+            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+              <span>Additional Options</span>
+              <a class="d-flex align-items-center text-muted" href="#">
+                <span data-feather="plus-circle"></span>
+              </a>
+            </h6>
+            <ul class="nav flex-column mb-2">
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="file-text"></span>
+                  My Profile
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div class="col-10 offset-2">
+          <router-view/>
+        </div>
       </div>
-    </nav>
-    <router-view/>
+    </div>
   </div>
 </template>
 
@@ -55,8 +60,6 @@
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
--- src/views/Note/AddNote.vue
+++ src/views/Note/AddNote.vue
@@ -1,7 +1,17 @@
 <template>
-    <div class="add-note">
-        <h1>Add a note</h1>
+  <div>
+    <h2>Add a note</h2>
+    <div class="row">
+      <div class="form-group col-10">
+        <label for="title">Note Title</label>
+        <input type="text" name="title" id="title" class="form-control">
+      </div>
+      <div class="form-group col-10">
+        <textarea name="body" id="body" cols="30" rows="10" class="form-control"></textarea>
+      </div>
     </div>
+    <button class="btn btn-success">Submit</button>
+  </div>
 </template>
 
 <script>
